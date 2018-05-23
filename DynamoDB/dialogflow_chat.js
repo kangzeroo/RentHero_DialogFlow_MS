171504712ef055fd2877a682644c6a38e3306a21
Kangze Huang <kangzeroo@gmail.com>
2018-05-23 08:06:40 -0400

    has snapsots now

--- DynamoDB/dialogflow_chat.js
+++ DynamoDB/dialogflow_chat.js
@@ -1,5 +1,6 @@
 const uuid = require('uuid')
 const moment = require('moment')
+const getAdSnapshot = require('../routes/Postgres/Queries/AdQueries').getAdSnapshot
 const insertItem = require('./general_insertions').insertItem
 const RENTHERO_COMM_LOGS = require('./schema/dynamodb_tablenames').RENTHERO_COMM_LOGS
 const RENTHERO_INTENTS_HIT = require('./schema/dynamodb_tablenames').RENTHERO_INTENTS_HIT
@@ -41,32 +42,42 @@ exports.saveDialog = function(message_id, ad_id, session_id, receiver_id, sender
 
 exports.saveIntentHit = function(identity_id, session_id, intent_id, intent_name, ad_id, prev_message_id, prev_message_text, next_message_id) {
   const p = new Promise((res, rej) => {
-    const timestamp = moment().toISOString()
-    const item = {
-      'TableName': RENTHERO_INTENTS_HIT,
-      'Item': {
-        'ITEM_ID': uuid.v4(),
-        'SESSION_ID': session_id,
-        'DATETIME': moment().toISOString(),
-        'AD_ID': ad_id,
-        'TENANT_ID': identity_id,
-        'INTENT_ID': intent_id,
-        'INTENT_NAME': intent_name,
-        'PREV_MESSAGE_ID': prev_message_id,
-        'PREV_MESSAGE_TEXT': prev_message_text,
-        'NEXT_MESSAGE_ID': next_message_id
+    getAdSnapshot(ad_id).then((data) => {
+      const item = {
+        'TableName': RENTHERO_INTENTS_HIT,
+        'Item': {
+          'ITEM_ID': uuid.v4(),
+          'SESSION_ID': session_id,
+          'DATETIME': moment().toISOString(),
+          'AD_ID': ad_id,
+          'TENANT_ID': identity_id,
+          'INTENT_ID': intent_id,
+          'INTENT_NAME': intent_name,
+          'PREV_MESSAGE_ID': prev_message_id,
+          'PREV_MESSAGE_TEXT': prev_message_text,
+          'NEXT_MESSAGE_ID': next_message_id,
+          'GPS_X': parseFloat(data.gps_x),
+          'GPS_Y': parseFloat(data.gps_y),
+          'AD_TITLE': data.ad_title,
+          'AD_UNIT': data.ad_unit,
+          'BATHROOMS': parseFloat(data.num_bathrooms),
+          'AVAIL_ROOMS': parseInt(data.num_available_rooms),
+          'AVAIL_ROOMS_PRICE': parseInt(data.available_rooms_price),
+          'TOTAL_ROOMS': parseInt(data.total_rooms),
+          'TOTAL_ROOMS_PRICE': parseInt(data.total_rooms_price)
+        }
       }
-    }
-    console.log(item)
-    insertItem(item)
-      .then((data) => {
-        // console.log(data)
-        res(data)
-      })
-      .catch((err) => {
-        // console.log(err)
-        rej(err)
-      })
+      console.log(item)
+      return insertItem(item)
+    })
+    .then((data) => {
+      // console.log(data)
+      res(data)
+    })
+    .catch((err) => {
+      // console.log(err)
+      rej(err)
+    })
   })
   return p
 }
--- test.js
+++ test.js
@@ -1,16 +1,20 @@
-const saveIntentLog = require('./api/stackdriver/stackdriver_api_dialogflow').saveIntentLog
-const getIntentLog = require('./api/stackdriver/stackdriver_api_dialogflow').getIntentLog
+// const saveIntentLog = require('./api/stackdriver/stackdriver_api_dialogflow').saveIntentLog
+// const getIntentLog = require('./api/stackdriver/stackdriver_api_dialogflow').getIntentLog
+//
+// const params = {
+//   autoPaginate: true,
+//   filter: `
+//     resource.type = "api"
+//     logName = "projects/dev-landlordai-8221e/logs/intents-hit"
+//     timestamp >= "2018-04-17T13:51:33+00:00" AND timestamp <= "2018-06-17T13:51:33+00:00"
+//     labels.intent_id = ("INIT---LookAtMeMom" OR "INIT---LookAtMeDad")
+//     jsonPayload.ad_id = "ad_id"
+//   `
+// }
+// getIntentLog('intents-hit', params)
+//
+// // saveIntentLog('identity_id', 'intent_id', 'session_id', 'bot_id', 'ad_id')
 
-const params = {
-  autoPaginate: true,
-  filter: `
-    resource.type = "api"
-    logName = "projects/dev-landlordai-8221e/logs/intents-hit"
-    timestamp >= "2018-04-17T13:51:33+00:00" AND timestamp <= "2018-06-17T13:51:33+00:00"
-    labels.intent_id = ("INIT---LookAtMeMom" OR "INIT---LookAtMeDad")
-    jsonPayload.ad_id = "ad_id"
-  `
-}
-getIntentLog('intents-hit', params)
+const getAdSnapshot = require('./routes/Postgres/Queries/AdQueries').getAdSnapshot
 
-// saveIntentLog('identity_id', 'intent_id', 'session_id', 'bot_id', 'ad_id')
+getAdSnapshot('d5cd68d9-fcc4-4abf-84e7-b0c52a8d9dbb')
